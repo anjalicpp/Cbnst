05cdf24d217cbf718b0c7bc92c5abae2c67339e1
Anjali <91792558+anjalicpp@users.noreply.github.com>
2022-10-31 20:55:35 +0530

    Update iteration code.c

--- lab-4:Iteration Method/iteration code.c
+++ lab-4:Iteration Method/iteration code.c
@@ -1,1 +1,36 @@
+Code for iteratiuon Method :
 
+#include<stdio.h>
+#include<math.h>
+#define f(x) cos(x)-(3*x)+1
+#define o(x) (cos(x)+1)/3.0
+#define O(x) (-sin(x)/3.0) 
+//#define f(x) (2*x)-log10(x)-7 
+int main()
+{
+    float x0,x1,e;
+    int i=0;
+    printf("Enter the value of x0 : ");
+    scanf("%f",&x0);
+    printf("Enter the value of allowed error : ");
+    scanf("%f",&e);
+    if(fabs(O(x0))<1)
+    {
+        printf("Roots found\n");
+    }
+    else
+    {
+        printf("Wrong value of x0\n");
+        return 0;
+    }
+    x1=x0;
+    do
+    {
+        x0=x1;
+        x1=o(x0);
+        i++;
+        printf("Root at iteration %d = %f\n",i,x1);
+    }while(fabs(x0-x1)>e);
+    printf("\nFinal root after %d iteration = %f\n",i,x1);
+    return 0;
+}
