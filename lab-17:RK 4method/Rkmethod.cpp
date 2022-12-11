83e0816de42307169f5f8977a46620333d90a41c
Anjali <91792558+anjalicpp@users.noreply.github.com>
2022-12-11 09:54:07 +0530

    Update Rkmethod.cpp

--- lab-17:RK 4method/Rkmethod.cpp
+++ lab-17:RK 4method/Rkmethod.cpp
@@ -1,1 +1,31 @@
 code:
+#include<stdio.h>
+#define F(x,y) (x-y)/(x+y)
+
+int main()
+{
+
+    int i,n;
+    float xo,yo,xn,h;
+    float k1,k2,k3,k4,k,x,y;
+    printf("Enter the values:xo,yo,h,xn:\n");
+    scanf("%f%f%f%f",&xo,&yo,&h,&xn);
+    n=(xn-xo)/h;
+    x=xo;
+    y=yo;
+
+    for(i=0;i<=n;i++)
+    {
+        k1=h*F(x,y);
+        k2=h*F(x+h/2.0,y+k1/2.0);
+        k3=h*F(x+h/2.0,y+k2/2.0);
+        k4=h*F(x+h,y+k3);
+        k=(k1+(k2+k3)*2.0+k4)/6.0;
+        printf("\n X=%f Y=%f",x,y);
+        x=x+h;
+        y=y+k;
+
+    }
+    return 0;
+
+}
